d5e201bb8f282b1c6a1c072d6f2ea5810bd0e452
Théo MARIE <theo.marie5@gmail.com>
2019-10-21 12:15:15 +0200

    Continued user creation feature [NOT COMPILING]

--- src/main/java/fr/esir/jxc/config/KafkaProducerConfig.java
+++ src/main/java/fr/esir/jxc/config/KafkaProducerConfig.java
@@ -11,7 +11,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.jxc.eventstoremanager.models.Event;
 
 @Configuration
 public class KafkaProducerConfig {
