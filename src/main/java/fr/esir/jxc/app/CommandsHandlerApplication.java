3039f19498d14dcfce051451e5a565381dc08cb5
Roland <rolandkouassi1@hotmail.fr>
2019-10-21 11:32:12 +0200

    init feature

--- src/main/java/fr/esir/jxc/app/CommandsHandlerApplication.java
+++ src/main/java/fr/esir/jxc/app/CommandsHandlerApplication.java
@@ -1,4 +1,8 @@
 package fr.esir.jxc.app;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CommandsHandlerApplication {
+
 }
---
+++ src/main/java/fr/esir/jxc/services/ArticleService.java
@@ -0,0 +1,8 @@
+package fr.esir.jxc.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+
+}
