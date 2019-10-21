ea2ab1c02385b30603d9f5737375ad3fb13a778c
Corentin Duchatelet <corentin.duchatelet@capgemini.com>
2019-10-21 11:48:55 +0200

    REST API Write Adding an article to a user

---
+++ src/main/java/fr/esir/jxc/controllers/ArticleController.java
@@ -0,0 +1,17 @@
+package fr.esir.jxc.controllers;
+
+import fr.esir.jxc.DTO.ArticleCreationDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/articles")
+public interface ArticleController {
+    /**
+     * @param body the ArticleCreationDTO
+     */
+    @PostMapping
+    public ResponseEntity createArticle(@RequestBody ArticleCreationDTO articleCreationDTO );
+}
---
+++ src/main/java/fr/esir/jxc/services/impl/ArticleServiceImpl.java
@@ -0,0 +1,22 @@
+package fr.esir.jxc.services.impl;
+
+import fr.esir.jxc.DTO.ArticleCreationDTO;
+import fr.esir.jxc.models.ArticleCreated;
+import fr.esir.jxc.services.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+    private final KafkaProducer kafkaProducer;
+
+    public ArticleServiceImpl(@Autowired KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    public void sendCreateArticleEvent(ArticleCreationDTO articleCreationDTO){
+        ArticleCreated articleCreated = ArticleCreated.of(articleCreationDTO);
+        this.kafkaProducer.produceObject(articleCreated);
+    }
+}
